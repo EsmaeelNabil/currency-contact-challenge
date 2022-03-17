76a8d76aa46722331c388d8f7d3ea04e4674bd11
esmaeelnabil <esmaeel.nabil@ibtikar.net.sa>
2022-03-17 11:15:48 +0200

    [REFACTOR] remove unused code

--- 1/bitpanda-challenage-one/app/src/main/java/com/esmaeel/challenge/common/Constants.kt
+++
@@ -1,8 +0,0 @@
-package com.esmaeel.challenge.common
-
-// TODO: Change this
-object Constants {
-    const val ALL = "Alle"
-    const val AVAILABLE = "Verfügbar"
-    const val FAVORITES = "Vorgemerkt"
-}
